ed298eafaebe363ed49cd033ada10167fe08bab2
AL <57809794+amandarae220@users.noreply.github.com>
2025-08-05 20:58:09 -0400

    Auto-generated commit

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { RootComponent } from './app/root/root.component';
-import { appConfig } from './app/app.config';
-
-bootstrapApplication(RootComponent, appConfig)
-  .catch(err => console.error(err));
