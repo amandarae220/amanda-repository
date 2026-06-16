38e00833b0634c6e1b1a200393e66f7d0baaf486
AL <amandarae19@gmail.com>
2026-06-16 09:41:41 -0400

    fixing failing test suite and dual-root-component bug

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+import { RootComponent } from './app/root/root.component';
 import { config } from './app/app.config.server';
 
-const bootstrap = (context: BootstrapContext) => bootstrapApplication(AppComponent, config, context);
+const bootstrap = (context: BootstrapContext) => bootstrapApplication(RootComponent, config, context);
 
 export default bootstrap;
